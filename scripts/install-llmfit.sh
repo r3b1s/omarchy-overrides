5a7776f2a83ce2d303205f5b5dce4cf345c7c13a
r3b1s <21206554+r3b1s@users.noreply.github.com>
2026-04-05 09:41:16 -0700

    Add llmfit installation script. `cargo` installed via locally cloned source

---
+++ scripts/install-llmfit.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+USER_HOME=$(eval echo "~${SUDO_USER:-$USER}")
+REPO_INSTALLS="$USER_HOME/Applications/repos"
+
+mkdir -p $REPO_INSTALLS
+cd $REPO_INSTALLS
+git clone https://github.com/AlexsJones/llmfit.git
+cd llmfit
+
+# Build from source and install to cargo PATH (~/.cargo/bin/llmfit)
+cargo install --path llmfit-tui
