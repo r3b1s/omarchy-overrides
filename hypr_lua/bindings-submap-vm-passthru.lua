e6ce96ab547972e6a79a40fa5e64bb39edaa8ec1
r3b1s <r3b1s@myparse.win>
2026-05-14 16:17:23 -0700

    fix: Use valid hyprland submap lua syntax

--- hypr_lua/bindings-submap-vm-passthru.lua
+++ hypr_lua/bindings-submap-vm-passthru.lua
@@ -2,18 +2,21 @@ local submapNotifyPassthru = 'notify-send "Enabled VM Passthrough" ""'
 local submapNotifyDefault = 'notify-send "Disabled VM Passthrough" ""'
 local delete = "code:119"
 
+-- Define: VM Passthru Submap
+hl.define_submap("passthru", function()
+	hl.bind("Delete", function()
+		hl.dispatch(hl.dsp.submap("reset"), { description = "End VM Passthru", submap = "passthru" })
+		hl.dispatch(hl.dsp.exec_cmd(submapNotifyDefault))
+	end)
+end)
+
+-- Bind: Enable VM Passthru Submap
 hl.unbind("SUPER + Delete")
 hl.unbind("SUPER + delete")
 hl.unbind("SUPER + DELETE")
 hl.unbind("SUPER + " .. delete)
-hl.bind(
-  "SUPER + Delete",
-  hl.dsp.exec_cmd("hyprctl dispatch 'hl.dsp.submap(\"passthru\")'; " .. submapNotifyPassthru),
-  { description = "Virtual Machine SUPER Key Passthru" }
-)
-
-hl.bind(
-  "Delete",
-  hl.dsp.exec_cmd("hyprctl dispatch 'hl.dsp.submap(\"reset\")'; " .. submapNotifyDefault),
-  { description = "End VM Passthru", submap = "passthru" }
-)
+--
+hl.bind("SUPER + Delete", function()
+	hl.dispatch(hl.dsp.submap("passthru"), { description = "Suppress user input during voxtype:type input" })
+	hl.dispatch(hl.dsp.exec_cmd(submapNotifyPassthru))
+end)
